1afc2d193a81a6eef15719de52e938abe3562c38
Andre Willik Valenti <awvalenti@gmail.com>
2013-06-25 14:47:16 -0300

    truco-versao-1

---
+++ truco-versao-1/webapp/js/src/Truco.js
@@ -0,0 +1,24 @@
+function Jogador() {
+  this._mao = [1, 2, 3];
+}
+
+Jogador.prototype.getMao = function() {
+  return this._mao;
+};
+
+function Truco() {
+  this._jogador1 = new Jogador();
+  this._jogador2 = new Jogador();
+}
+
+Truco.prototype.getMaoJogador1 = function() {
+  return this._jogador1.getMao();
+};
+
+Truco.prototype.getMaoJogador2 = function() {
+  return this._jogador2.getMao();
+};
+
+Truco.prototype.jogarUmaCarta = function() {
+  return this._jogador1._mao.pop();
+};
